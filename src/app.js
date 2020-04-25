fa223b1837b1433198d36651c2f42b776af679ea
Lyashka <iuliana9864@gmail.com>
2020-04-25 18:52:59 +0300

    change stub to dynamic canvas

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 import '/index.scss';
+
+const WIDTH_CANVAS = 1040;
+const HEIGHT_CANVAS = 598;
+const COLOR_CANVAS = '#fff';
+const CANVAS = document.querySelector(".work-display__plan");
+const CONTEXT = CANVAS.getContext('2d');
+
+CANVAS.width = WIDTH_CANVAS;
+CANVAS.height = HEIGHT_CANVAS;
+CONTEXT.fillStyle = COLOR_CANVAS;
+CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
