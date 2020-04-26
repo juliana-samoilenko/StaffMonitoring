b9560f0c2220efbfa78630c3e2f2c39aa4a2500a
Lyashka <iuliana9864@gmail.com>
2020-04-26 09:37:46 +0300

    initialization two.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import '/index.scss';
+import 'two.js';
 
-const WIDTH_CANVAS = 1040;
+var elem = document.getElementById('work-display__plan');
+var two = new Two({ width: 1040, height: 598 }).appendTo(elem);
+var circle = two.makeCircle(72, 100, 50);
+var rect = two.makeRectangle(213, 100, 100, 100);
+
+circle.fill = '#FF8000';
+circle.stroke = 'orangered';
+circle.linewidth = 5;
+
+rect.fill = 'rgb(0, 200, 255)';
+rect.opacity = 0.75;
+rect.noStroke();
+
+two.update();
+
+/*const WIDTH_CANVAS = 1040;
 const HEIGHT_CANVAS = 598;
 const COLOR_CANVAS = '#fff';
 const CANVAS = document.querySelector(".work-display__plan");
@@ -9,4 +25,4 @@ const CONTEXT = CANVAS.getContext('2d');
 CANVAS.width = WIDTH_CANVAS;
 CANVAS.height = HEIGHT_CANVAS;
 CONTEXT.fillStyle = COLOR_CANVAS;
-CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
+CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);*/
