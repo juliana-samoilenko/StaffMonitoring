4bbb268d2b461c3c40cdb39469a3b736a7786d16
Lyashka <iuliana9864@gmail.com>
2020-04-27 18:26:30 +0300

    add class Door with constructor

---
+++ src/Door.js
@@ -0,0 +1,14 @@
+import {
+  PLAN
+} from './app.js';
+
+export default class Door {
+  constructor(xStart, yStart, xEnd, yEnd, thickness, color) {
+    this.xStart = xStart;
+    this.yStart = yStart;
+    this.xEnd = xEnd;
+    this.yEnd = yEnd;
+    this.thickness = thickness;
+    this.color = color;
+  }
+}
