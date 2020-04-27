7a74b1fe3441967fdc2ddf8d06a408cde7f25be6
Lyashka <iuliana9864@gmail.com>
2020-04-27 16:50:22 +0300

    drawing walls of building

--- src/app.js
+++ src/app.js
@@ -11,4 +11,10 @@ import {
 export const CANVAS = document.getElementById('work-display__plan');
 export const PLAN = new Two({width: WIDTH_CANVAS, height: HEIGHT_CANVAS, domElement: CANVAS});
 
+ELEMENTS_BUILDING.walls.forEach((item) => {
+  let wall = new Wall(item.xStart, item.yStart, 
+                      item.xEnd, item.yEnd, 
+                      item.thickness, item.color);
+  wall.drawWall();
+})
 PLAN.update();
