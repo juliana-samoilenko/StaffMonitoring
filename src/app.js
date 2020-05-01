31a9571e18b7131fd8bcf99f7b3b54a2b369d7c2
Lyashka <iuliana9864@gmail.com>
2020-05-01 10:08:05 +0300

    add class Zone to draw

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import '/index.scss';
 import Two from 'two.js';
 import Wall from './Wall.js';
 import Door from './Door.js';
+import Zone from './Zone.js';
 
 import {
   WIDTH_CANVAS,
@@ -14,9 +15,10 @@ const canvas = new Two({width: WIDTH_CANVAS, height: HEIGHT_CANVAS, domElement: 
 const buildingObjectsByTypes = {
   wall: Wall,
   door: Door,
+  zone: Zone,
 };
 
-[...ELEMENTS_BUILDING.walls, ...ELEMENTS_BUILDING.doors].forEach((item) => {
+[...ELEMENTS_BUILDING.walls, ...ELEMENTS_BUILDING.doors, ...ELEMENTS_BUILDING.zones].forEach((item) => {
   const Drawable = buildingObjectsByTypes[item.type];
   let propertiesForRendering = [];
   for (let key in item) {
