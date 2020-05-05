76d2a7554bb97c0534f65ff57bc1673a5ad5f6a5
Lyashka <iuliana9864@gmail.com>
2020-05-05 17:51:24 +0300

    add class composition

--- src/Employee.js
+++ src/Employee.js
@@ -1,10 +1,29 @@
-import {DrawableCircle} from './Drawable.js';
+import {DrawableCircle} from './Drawable/DrawableCircle';
+import {Moveable} from './Moveable.js';
 
-export class Employee extends DrawableCircle {
-  constructor(id, name, xCurrent, yCurrent, radius, color, two, drawPoint, track) {
-    super(id, name, xCurrent, yCurrent, radius, color, two, drawPoint);
-    this.track = track;
-    this.currentPointIndex = 0;
+export class Employee extends Moveable(DrawableCircle) {
+  constructor({
+    id, 
+    name, 
+    xCurrent, 
+    yCurrent, 
+    radius, 
+    color, 
+    drawPoint, 
+    track, 
+    currentPointIndex,
+  }, two) {
+    super({
+      id,
+      name,
+      xCurrent,
+      yCurrent,
+      radius,
+      color,
+      drawPoint,
+      track,
+      currentPointIndex,
+    }, two);
   }
 
   clear() {
@@ -16,34 +35,5 @@ export class Employee extends DrawableCircle {
     this.xCenter = x;
     this.yCenter = y;
     super.draw();
-  }
-
-  getPoint() {
-
-  }
-
-
-  moveAlong(track) {
-    this.track = track.points;
-    //легко отследить индекс точки пути
-    /*for(let i = 0; i < this.track.length; i++) {
-      this.currentPointIndex = i;
-      let xNext = this.track[i].x;
-      let yNext = this.track[i].y;
-
-      setInterval(()=> {
-        this.move(xNext, yNext)
-      }, 5000);
-    }*/
-
-    this.track.forEach((point) => {
-      const xNext = point.x;
-      const yNext = point.y;
-
-      const id = setInterval(() => this.move(xNext, yNext), 100);
-      
-      //просто, чтобы остановить
-      setTimeout(() => clearInterval(id), 5000)
-    });
   }
 }
