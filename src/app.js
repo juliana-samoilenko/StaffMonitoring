c614c04065af47d067bf3712fba206c720e3113a
Lyashka <iuliana9864@gmail.com>
2020-05-05 17:52:37 +0300

    change variable

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import '/index.scss';
 import Two from 'two.js';
-import {Wall} from './Wall.js';
-import {Door} from './Door.js';
-import {Zone} from './Zone.js';
-import {Employee} from './Employee.js';
+import { Wall } from './Wall.js';
+import { Door } from './Door.js';
+import { Zone } from './Zone.js';
+import { Employee } from './Employee.js';
 import { EmployeeTrack } from './EmployeeTrack.js';
 
 import {
@@ -11,7 +11,6 @@ import {
   HEIGHT_CANVAS,
   ELEMENTS_BUILDING,
 } from '/const.js';
-
 
 const canvasElement = document.querySelector('.js-building-canvas');
 const two = new Two( { width: WIDTH_CANVAS, height: HEIGHT_CANVAS, domElement: canvasElement } );
@@ -30,8 +29,10 @@ const buildingObjectsByTypes = {
   drawableObject.draw();
 });
 
-let track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 100, y: 50}, {x: 100, y: 100}, {x: 200, y: 200}, {x: 300, y: 200}], employeeCurrentPointIndex: 0} );
+const track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 50, y: 100}, {x: 100, y: 100}, {x: 100, y: 150}, {x: 100, y: 100}, {x: 50, y: 100}]} );
 
-let circle = new Employee( {id: 13, name: 'Юдашин В.К.', xCurrent: 100, yCurrent: 100, radius: 10, color: "red"}, two );
-circle.draw();
-circle.moveAlong(track);
+const employee = new Employee({
+  id: 13, name: 'Юдашин В.К.', xCurrent: 100, yCurrent: 100, radius: 10, color: "red", track,
+}, two );
+employee.draw();
+employee.moveAlong();
