5726b864e6decce4cd41d3853a8581eb933428b5
Lyashka <iuliana9864@gmail.com>
2020-05-05 17:42:06 +0300

    structuring Drawable interface

---
+++ src/Drawable/DrawableLine.js
@@ -0,0 +1,22 @@
+export class DrawableLine {
+  constructor({ xStart, yStart, xEnd, yEnd, thickness, color }, two) {
+    this.xStart = xStart;
+    this.yStart = yStart;
+    this.xEnd = xEnd;
+    this.yEnd = yEnd;
+    this.thickness = thickness;
+    this.color = color;
+    this.two = two;
+  }
+
+  draw() {
+    this._makeLine();
+    this.two.update();
+  }
+
+  _makeLine() {
+    const line = this.two.makePath(this.xStart, this.yStart, this.xEnd, this.yEnd);
+    line.linewidth = this.thickness;
+    line.stroke = this.color;
+  }
+}
