6118ec759b8695992e825a8822873fcc6119f875
Lyashka <iuliana9864@gmail.com>
2020-05-08 14:42:04 +0300

    add test when add track to employee

--- src/employee.test.js
+++ src/employee.test.js
@@ -18,4 +18,18 @@ describe('Emloyee class, check logic', function() {
       expect(employee.track._points).toEqual([]);
     });
   });
+
+  describe('Add track', function() {
+    it('When adding a track to an employee, it is saved in the employee property', () => {
+      //Arrange
+      const track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 50, y: 100}]} );
+
+      //Act
+      const employee = new Employee({
+        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two );
+
+      //Assert
+      expect(employee.track._points).toEqual([{x: 50, y: 50}, {x: 50, y: 100}]);
+    });
+  });
 });
