e0bfd6db72798ca64bd5bdfa1733fa58e17bc78d
Vitaly Krenel <vitaly@clickclickdrive.net>
2020-05-09 20:22:40 +1100

    Apply best practices to existent tests to improve their readability

--- src/employee.test.js
+++ src/employee.test.js
@@ -1,101 +1,114 @@
-import {Employee} from './Employee';
-import {EmployeeTrack} from './EmployeeTrack';
+import { Employee } from './Employee';
+import { EmployeeTrack } from './EmployeeTrack';
 
-describe('Emloyee class, check logic', function() {
-
-  describe('Add empty track', function() {
+describe('Emloyee class, check logic', () => {
+  describe('Add empty track', () => {
     it('When adding a empty track to an employee, it is saved in the employee property', () => {
-      //Arrange
+      // Arrange
       const two = {};
-      const track = new EmployeeTrack( { id: 2, points: []} );
+      const track = new EmployeeTrack({ id: 2, points: [] });
 
-      //Act
+      // Act
       const employee = new Employee({
-        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two );
+        id: 1,
+        name: 'Петров С.М.',
+        xCurrent: 50,
+        yCurrent: 50,
+        radius: 15,
+        color: '#3A19A4',
+        track,
+      }, two);
 
-      //Assert
-      expect(employee.track._points).toEqual([]);
+      // Assert
+      expect(employee.track).toBe(track);
     });
   });
 
-  describe('Add track', function() {
+  describe('Add track', () => {
     it('When adding a track to an employee, it is saved in the employee property', () => {
-      //Arrange
-      const two = {};
-      const track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 50, y: 100}]} );
+      // Act
+      const track = new EmployeeTrack({ id: 1, points: [{ x: 50, y: 50 }, { x: 50, y: 100 }] });
 
-      //Act
-      const employee = new Employee({
-        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two );
-
-      //Assert
-      expect(employee.track._points).toEqual([{x: 50, y: 50}, {x: 50, y: 100}]);
+      // Assert
+      expect(track.getPoint(0)).toEqual({ x: 50, y: 50 });
+      expect(track.getPoint(1)).toEqual({ x: 50, y: 100 });
     });
   });
 
-  describe('Add current coordinates to employee', function() {
+  describe('Add current coordinates to employee', () => {
     it('When coordinates are added to an employee, they are saved in the employee object', () => {
-      //Arrange
+      // Arrange
       const two = {};
       const x = 300;
       const y = 400;
 
-      //Act
+      // Act
       const employee = new Employee({
-        id: 1, name: 'Петров С.М.', xCurrent: x, yCurrent: y, radius: 15, color: '#3A19A4', track: {}}, two );
+        id: 1, name: 'Петров С.М.', xCurrent: x, yCurrent: y, radius: 15, color: '#3A19A4', track: {},
+      }, two);
 
-      //Assert
+      // Assert
       expect(employee.xCenter).toEqual(300);
       expect(employee.yCenter).toEqual(400);
     });
   });
 
-  describe('Add move to employee', function() {
+  describe('Add move to employee', () => {
     it('When using move(x, y) method, the values of the starting point are changed to x and y', () => {
-      //Arrange
-      const two = {};
-      const employee = new Employee({
-        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: {}}, two );
-      const x = 300; 
-      const y = 500;
+      // Arrange
+      const two = {
+        remove: jest.fn(),
+      };
 
-      //Act
+      const employee = new Employee({
+        id: 1,
+        name: 'Петров С.М.',
+        xCurrent: 50,
+        yCurrent: 50,
+        radius: 15,
+        color: '#3A19A4',
+        track: {},
+      }, two);
+      const x = 300;
+      const y = 500;
+      employee.draw = jest.fn();
+
+      // Act
       employee.move(x, y);
 
-      //Assert
+      // Assert
       expect(employee.xCenter).toEqual(300);
       expect(employee.yCenter).toEqual(500);
     });
   });
 
-  describe('Add track', function() {
+  describe('Add track', () => {
     it('Check track length', () => {
-      //Arrange
-      const two = {};
-      const track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 50, y: 100}, {x: 100, y: 100}, {x: 100, y: 150}, {x: 100, y: 100}, {x: 50, y: 100}]} );
-      const employee = new Employee({
-        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two )
+      // Arrange
+      const points = [{ x: 50, y: 50 }, { x: 50, y: 100 }, { x: 100, y: 100 }, { x: 100, y: 150 }, { x: 100, y: 100 }, { x: 50, y: 100 }];
+      const track = new EmployeeTrack({ id: 1, points });
 
-      //Act
-      const trackLength = employee.track.getLength();
+      // Act
+      const trackLength = track.getLength();
 
-      //Assert
-      expect(trackLength).toEqual(6);
+      // Assert
+      expect(trackLength).toEqual(points.length);
     });
   });
 
-  describe('Add track', function() {
-    it('Check to change _currentPointIndex', () => {
-      //Arrange
+  describe('Add track', () => {
+    it('When next point received for the first time, check that _currentPointIndex is changed to 1', () => {
+      // Arrange
       const two = {};
-      const track = new EmployeeTrack( { id: 1, points: [{x: 60, y: 100}, {x: 700, y: 600}]} );
+      const track = new EmployeeTrack({ id: 1, points: [{ x: 60, y: 100 }, { x: 700, y: 600 }] });
       const employee = new Employee({
-        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two );
+        id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track,
+      }, two);
 
-      //Act
+      // Act
       const { x: xNext, y: yNext } = employee.getNextPoint();
 
-      //Assert
+      // Assert
       expect(xNext).toEqual(700);
       expect(yNext).toEqual(600);
       expect(employee._currentPointIndex).toEqual(1);
