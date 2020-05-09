40269caf04e759559781c3a035af7677787b51e2
Lyashka <iuliana9864@gmail.com>
2020-05-09 09:06:02 +0300

    separate canvas for each test

--- src/employee.test.js
+++ src/employee.test.js
@@ -1,13 +1,12 @@
 import {Employee} from './Employee';
 import {EmployeeTrack} from './EmployeeTrack';
 
-const two = {};
-
 describe('Emloyee class, check logic', function() {
 
   describe('Add empty track', function() {
     it('When adding a empty track to an employee, it is saved in the employee property', () => {
       //Arrange
+      const two = {};
       const track = new EmployeeTrack( { id: 2, points: []} );
 
       //Act
@@ -22,6 +21,7 @@ describe('Emloyee class, check logic', function() {
   describe('Add track', function() {
     it('When adding a track to an employee, it is saved in the employee property', () => {
       //Arrange
+      const two = {};
       const track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 50, y: 100}]} );
 
       //Act
@@ -36,6 +36,7 @@ describe('Emloyee class, check logic', function() {
   describe('Add current coordinates to employee', function() {
     it('When coordinates are added to an employee, they are saved in the employee object', () => {
       //Arrange
+      const two = {};
       const x = 300;
       const y = 400;
 
@@ -52,6 +53,7 @@ describe('Emloyee class, check logic', function() {
   describe('Add move to employee', function() {
     it('When using move(x, y) method, the values of the starting point are changed to x and y', () => {
       //Arrange
+      const two = {};
       const employee = new Employee({
         id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: {}}, two );
       const x = 300; 
@@ -69,6 +71,7 @@ describe('Emloyee class, check logic', function() {
   describe('Add track', function() {
     it('Check track length', () => {
       //Arrange
+      const two = {};
       const track = new EmployeeTrack( { id: 1, points: [{x: 50, y: 50}, {x: 50, y: 100}, {x: 100, y: 100}, {x: 100, y: 150}, {x: 100, y: 100}, {x: 50, y: 100}]} );
       const employee = new Employee({
         id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two )
@@ -84,6 +87,7 @@ describe('Emloyee class, check logic', function() {
   describe('Add track', function() {
     it('Check to change _currentPointIndex', () => {
       //Arrange
+      const two = {};
       const track = new EmployeeTrack( { id: 1, points: [{x: 60, y: 100}, {x: 700, y: 600}]} );
       const employee = new Employee({
         id: 1, name: 'Петров С.М.', xCurrent: 50, yCurrent: 50, radius: 15, color: '#3A19A4', track: track}, two );
