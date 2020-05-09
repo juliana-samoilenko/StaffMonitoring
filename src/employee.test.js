3f354c0dbc053764ba4e0ba620fc9aa1620d7668
Lyashka <iuliana9864@gmail.com>
2020-05-09 20:04:06 +0300

    change validation of passed values

--- src/employee.test.js
+++ src/employee.test.js
@@ -27,11 +27,13 @@ describe('Emloyee class, check logic', () => {
   describe('Add track', () => {
     it('When adding a track to an employee, it is saved in the employee property', () => {
       // Act
-      const track = new EmployeeTrack({ id: 1, points: [{ x: 50, y: 50 }, { x: 50, y: 100 }] });
+      const point1 = { x: 600, y: 50 };
+      const point2 = { x: 50, y: 100 };
+      const track = new EmployeeTrack({ id: 1, points: [point1, point2] });
 
       // Assert
-      expect(track.getPoint(0)).toEqual({ x: 50, y: 50 });
-      expect(track.getPoint(1)).toEqual({ x: 50, y: 100 });
+      expect(track.getPoint(0)).toEqual({ x: point1.x, y: point1.y });
+      expect(track.getPoint(1)).toEqual({ x: point2.x, y: point2.y });
     });
   });
 
