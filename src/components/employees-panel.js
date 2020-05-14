f3a29d7f8d4ac4f97ba1ee448e69fcd67576b581
Lyashka <iuliana9864@gmail.com>
2020-05-14 22:48:15 +0300

    render employees panel with JS

---
+++ src/components/employees-panel.js
@@ -0,0 +1,29 @@
+export const createEmployeesPanelTemplate = (employeesList) => 
+`<section class="employee-information-panel">
+<div class="employees-panel">
+
+  <header class="employees-panel__header list-header">
+    <h2 class="list-header__text">Список сотрудников</h2>
+    <button class="list-header__button-close button-close" type="button" title="Закрыть">&#10006;</button>
+  </header>
+
+  <div class="employees-panel__body employees-list">
+    <ul class="employees-list__container js-employees-list__container">
+      ${employeesList}
+    </ul>
+  </div>
+
+  <footer class="employees-panel__footer footer-list">
+    <button class="footer-list__button-open-add" type="button" title="Добавить сотрудика"></button>
+  </footer>
+</div>
+</section>`;
+
+export const createEmployeeListItemTemplate = (employees) => 
+`<li class="employees-list__item employee">
+<div class="employee__container">
+  <span class="employee__name">${employees.id}) ${employees.name}</span>
+  <button class="employee__button-open-edit" type="button" title="Редактировать">
+  </button>
+</div>
+</li>`;
--- src/renderApp.js
+++ src/renderApp.js
@@ -1,6 +1,7 @@
 import { createCanvasTemplate } from './components/canvas';
 import { createNotificationListTemplate, createNotificationItemTemplate } from './components/notification';
 import { createButtonOpenListTemplate } from './components/button-open-list-employees';
+import { createEmployeesPanelTemplate, createEmployeeListItemTemplate } from './components/employees-panel';
 
 export const renderApp = () => {
   const mainContainer = document.querySelector('.js-work-display');
@@ -9,9 +10,13 @@ export const renderApp = () => {
   /*const violationInformation = [{ name: 'Миронов И.А', zone: 'Цех 1' }, { name: 'Петухов В.П.', zone: 'Высотные работы' }, { name: 'Лукин В.Р', zone: 'Цех 1' }];
   const notificationItemTemplates = violationInformation.map(e => createNotificationItemTemplate(e)).join('');
 
-  render(mainContainer, createNotificationListTemplate(notificationItemTemplates), 'beforeend');*/
+  render(mainContainer, createNotificationListTemplate(notificationItemTemplates), 'beforeend');
+  
+  render(mainContainer, createButtonOpenListTemplate(), 'beforeend');*/
+  const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
+  const employeeListItemTemplates = employeesList.map(e => createEmployeeListItemTemplate(e)).join('');
 
-  render(mainContainer, createButtonOpenListTemplate(), 'beforeend');
+  render(mainContainer, createEmployeesPanelTemplate(employeeListItemTemplates), 'beforeend');
 }
 
 const render = (container, template, position) => {
