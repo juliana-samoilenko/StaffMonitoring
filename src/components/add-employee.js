d9708942abb2c269b9bdf2a1478edb20a7000f3b
Lyashka <iuliana9864@gmail.com>
2020-05-15 10:33:17 +0300

    render add-employee with JS

---
+++ src/components/add-employee.js
@@ -0,0 +1,53 @@
+export const createAddEmployeePanelTemplate = (form) => `<div class="employee-add-panel">
+
+<header class="employee-add-panel__header">
+  <button class="employee-add-panel__button button-close" type="button" title="Закрыть">&#10006;</button>
+</header>
+
+<div class="employee-add-panel__body">
+  ${form}
+</div>
+
+<footer class="employee-add-panel__footer footer-add">
+  <button type="button" class="footer-add__button-add-employee" title="Добавить работника">Добавить сотрудника</button>
+  </footer>
+</form>
+</div>`;
+
+export const createEmployeeAddFormTemplate = (tracksList, zonesList) => `<form class="employee-add-panel__form add-employee-form" action="" name="add-emp" method="GET">
+
+<div class="add-employee-form__name add-name-container">
+  <label class="add-name-container__label" for="add-name">ФИО:</label>
+  <input class="add-name-container__input" type="text" id="add-name" placeholder="Иванов И.И." autofocus
+    required>
+</div>
+
+<div class="add-employee-form__position add-position-container">
+  <label class="add-position-container__label" for="add-position">Должность:</label>
+  <input class="add-position-container__input" type="text" id="add-position" placeholder="Техник"
+    required>
+</div>
+
+<div class="add-employee-form__track add-track-container">
+  <label class="add-track-container__label" for="add-track">Путь:</label>
+  <select class="add-track-container__select" id="add-track" required>
+    <option class="js-add-tracks">Выберите путь</option>
+    ${tracksList}
+  </select>
+</div>
+
+<label>Доступные зоны:</label>
+<div class="add-employee-form__zone">
+  <div class="add-zone-container">
+    ${zonesList}
+  </div>
+</div>
+</form>`;
+
+
+export const createTracksList = (tracks) => `<option value="${tracks.id}">${tracks.id}</option>`;
+
+export const createZonesList = (zones) => `<div>
+  <input type="checkbox" id=zone"${zones.id}">
+  <label for="zone"${zones.id}">${zones.name}</label>
+</div>`;
--- src/renderApp.js
+++ src/renderApp.js
@@ -2,6 +2,7 @@ import { createCanvasTemplate } from './components/canvas';
 import { createNotificationListTemplate, createNotificationItemTemplate } from './components/notification';
 import { createButtonOpenListTemplate } from './components/button-open-list-employees';
 import { createEmployeesListPanelTemplate, createEmployeeListItemTemplate } from './components/employees-list-panel';
+import { createAddEmployeePanelTemplate, createEmployeeAddFormTemplate, createTracksList, createZonesList } from './components/add-employee';
 
 export const renderApp = () => {
   const mainContainer = document.querySelector('.js-work-display');
@@ -16,6 +17,15 @@ export const renderApp = () => {
   const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
   const employeeListItemTemplates = employeesList.map(e => createEmployeeListItemTemplate(e)).join('');
   render(mainContainer, createEmployeesListPanelTemplate(employeeListItemTemplates), 'beforeend');
+
+  const employeePanel = mainContainer.querySelector('.js-employee-information-panel');
+  const tracks = [{ id: 1, points: [{}, {}]}, { id: 2, points: [{}, {}]}, { id: 4, points: [{}, {}]}];
+  const tracksList = tracks.map(e => createTracksList(e)).join('');
+  const zones = [ { id: 1, name:'Цех 1' }, { id: 2, name:'Высотные работы' }, { id: 3, name:'Цех 2' }, { id: 4, name:'Напряжение' } ];
+  const zonesList = zones.map(e => createZonesList(e)).join('');
+  const form = createEmployeeAddFormTemplate(tracksList, zonesList);
+
+  render (employeePanel, createAddEmployeePanelTemplate(form), 'beforeend');
 }
 
 const render = (container, template, position) => {
