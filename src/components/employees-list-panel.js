3edbe61e4193d8cf98a2c86b43e2270f5e0d5c8d
Lyashka <iuliana9864@gmail.com>
2020-05-16 17:08:34 +0300

    change the structure of the employee list panel (separate buttons, rendering of each employee inside)

--- src/components/employees-list-panel.js
+++ src/components/employees-list-panel.js
@@ -1,30 +1,38 @@
-export const createEmployeesListPanelTemplate = (employeesList) => `<section class="employee-information-panel js-employee-information-panel">
-<div class="employees-panel">
+export const createTemplateForEmployeesListPanel = (employeesList) => {
+  const employeeListItemTemplates = employeesList.map(e => createTemplateForEmployeeListItem(e)).join('');
 
-  <header class="employees-panel__header list-header">
-    <h2 class="list-header__text">Список сотрудников</h2>
-    <button class="list-header__button-close button-close" type="button" title="Закрыть">&#10006;</button>
-  </header>
-
-  <div class="employees-panel__body employees-list">
-    <ul class="employees-list__container js-employees-list__container">
-      ${employeesList}
-    </ul>
+  return `<section class="employee-information-panel js-employee-information-panel">
+  <div class="employees-panel">
+  
+    <header class="employees-panel__header list-header">
+      <h2 class="list-header__text">Список сотрудников</h2>
+      ${createTemplateForCloseEmployeeListButton()}
+    </header>
+  
+    <div class="employees-panel__body employees-list">
+      <ul class="employees-list__container js-employees-list__container">
+        ${employeeListItemTemplates}
+      </ul>
+    </div>
+  
+    <footer class="employees-panel__footer footer-list">
+      ${createTemlateForAddEmployeeButton()}
+    </footer>
   </div>
+  </section>`;
+}
 
-  <footer class="employees-panel__footer footer-list">
-    <button class="footer-list__button-open-add" type="button" title="Добавить сотрудика"></button>
-  </footer>
-</div>
-</section>`;
-
-export const createEmployeeListItemTemplate = (employees) => `<li class="employees-list__item employee">
+const createTemplateForEmployeeListItem = (employees) => `<li class="employees-list__item employee">
 <div class="employee__container">
   <span class="employee__name">${employees.id}) ${employees.name}</span>
   <button class="employee__button-open-edit" type="button" title="Редактировать">
   </button>
 </div>
 </li>`;
+
+const createTemplateForCloseEmployeeListButton = () => `<button class="list-header__button-close button-close" type="button" title="Закрыть">&#10006;</button>`;
+
+const createTemlateForAddEmployeeButton = () => `<button class="footer-list__button-open-add" type="button" title="Добавить сотрудика"></button>`;
 
 export const createTemplateForOpenEmployeeListButton = () => `<button class="button button-open-list" type="button" title="Список сотрудников">
 </button>`;
--- src/renderApp.js
+++ src/renderApp.js
@@ -1,6 +1,6 @@
 import { createCanvasTemplate } from './components/canvas';
 import { createNotificationListTemplate} from './components/notification';
-import { createTemplateForOpenEmployeeListButton, createEmployeesListPanelTemplate, createEmployeeListItemTemplate } from './components/employees-list-panel';
+import { createTemplateForOpenEmployeeListButton, createTemplateForEmployeesListPanel } from './components/employees-list-panel';
 import { createAddEmployeePanelTemplate, createEmployeeAddFormTemplate, createTracksList, createZonesList } from './components/add-employee';
 
 export const renderApp = () => {
@@ -10,13 +10,12 @@ export const renderApp = () => {
   const violationInformation = [{ name: 'Миронов И.А', zone: 'Цех 1' }, { name: 'Петухов В.П.', zone: 'Высотные работы' }, { name: 'Лукин В.Р', zone: 'Цех 1' }];
   
   render(rootContainer, createNotificationListTemplate(violationInformation), 'beforeend');
-  render(rootContainer, createTemplateForOpenEmployeeListButton(), 'beforeend');
+  /*render(rootContainer, createTemplateForOpenEmployeeListButton(), 'beforeend');*/
 
-  /*const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
-  const employeeListItemTemplates = employeesList.map(e => createEmployeeListItemTemplate(e)).join('');
-  render(rootContainer, createEmployeesListPanelTemplate(employeeListItemTemplates), 'beforeend');
+  const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
+  render(rootContainer, createTemplateForEmployeesListPanel(employeesList), 'beforeend');
 
-  const employeePanel = rootContainer.querySelector('.js-employee-information-panel');*/
+  const employeePanel = rootContainer.querySelector('.js-employee-information-panel');
   /*const tracks = [{ id: 1, points: [{}, {}]}, { id: 2, points: [{}, {}]}, { id: 4, points: [{}, {}]}];
   const tracksList = tracks.map(e => createTracksList(e)).join('');
   const zones = [ { id: 1, name: 'Цех 1' }, { id: 2, name: 'Высотные работы' }, { id: 3, name: 'Цех 2' }, { id: 4, name: 'Напряжение' } ];
