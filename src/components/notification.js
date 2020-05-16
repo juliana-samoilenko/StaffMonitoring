e89f0e5435866e078eb61e7baf3441a25b176aeb
Lyashka <iuliana9864@gmail.com>
2020-05-16 15:29:06 +0300

    add import image svg

--- src/components/notification.js
+++ src/components/notification.js
@@ -1,6 +1,8 @@
+import warningSvg from '../img/warning.min.svg';
+
 const createNotificationItemTemplate = (violation) => `<article class="notification-list__notification notification">
   <header class="notification__header">
-    <img class="notification__img" src="/img/warning.min.svg" arial-role="presentation">
+    <img class="notification__img" src="${warningSvg}" arial-role="presentation">
     <p class="notification__text">Внимание!</p>
     <button class="notification__button-close" type="button" title="Закрыть">&#10006;</button>
   </header>
