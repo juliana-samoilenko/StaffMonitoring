4b3aafa5ffd0461146bfd9f02db6854465bf7bed
Lyashka <iuliana9864@gmail.com>
2020-05-16 05:53:12 +0300

    rename root element page

--- src/renderApp.js
+++ src/renderApp.js
@@ -5,19 +5,19 @@ import { createEmployeesListPanelTemplate, createEmployeeListItemTemplate } from
 import { createAddEmployeePanelTemplate, createEmployeeAddFormTemplate, createTracksList, createZonesList } from './components/add-employee';
 
 export const renderApp = () => {
-  const mainContainer = document.querySelector('.js-work-display');
-  render(mainContainer, createCanvasTemplate(), 'afterbegin');
+  const rootContainer = document.querySelector('.js-work-display');
+  render(rootContainer, createCanvasTemplate(), 'afterbegin');
 
   const violationInformation = [{ name: 'Миронов И.А', zone: 'Цех 1' }, { name: 'Петухов В.П.', zone: 'Высотные работы' }, { name: 'Лукин В.Р', zone: 'Цех 1' }];
   
-  render(mainContainer, createNotificationListTemplate(violationInformation), 'beforeend');
-  render(mainContainer, createButtonOpenListTemplate(), 'beforeend');
+  render(rootContainer, createNotificationListTemplate(violationInformation), 'beforeend');
+  render(rootContainer, createButtonOpenListTemplate(), 'beforeend');
 
   /*const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
   const employeeListItemTemplates = employeesList.map(e => createEmployeeListItemTemplate(e)).join('');
-  render(mainContainer, createEmployeesListPanelTemplate(employeeListItemTemplates), 'beforeend');
+  render(rootContainer, createEmployeesListPanelTemplate(employeeListItemTemplates), 'beforeend');
 
-  const employeePanel = mainContainer.querySelector('.js-employee-information-panel');*/
+  const employeePanel = rootContainer.querySelector('.js-employee-information-panel');*/
   /*const tracks = [{ id: 1, points: [{}, {}]}, { id: 2, points: [{}, {}]}, { id: 4, points: [{}, {}]}];
   const tracksList = tracks.map(e => createTracksList(e)).join('');
   const zones = [ { id: 1, name: 'Цех 1' }, { id: 2, name: 'Высотные работы' }, { id: 3, name: 'Цех 2' }, { id: 4, name: 'Напряжение' } ];
