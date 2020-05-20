6a451879f79c22cae2a70da6368eb9c6a64056a7
Lyashka <iuliana9864@gmail.com>
2020-05-20 11:08:08 +0300

    change rendering

--- src/renderApp.js
+++ src/renderApp.js
@@ -1,27 +1,39 @@
-import { createCanvasTemplate } from './components/canvas';
-import { createNotificationListTemplate} from './components/notification';
-import { createTemplateForOpenEmployeeListButton, createTemplateForEmployeesPanel } from './components/employees-list-panel';
+import { Canvas } from './components/Canvas';
+import { Notification } from './components/Notification';
+import { OpenEmployeeListPanelButton } from './components/OpenEmployeeListPanelButton';
+import { createTemplateForEmployeesPanel } from './components/employees-list-panel';
 import { createAddEmployeePanelTemplate, createTemplateForAddEmployeeForm } from './components/add-employee-panel';
 import { createEditEmployeePanelTemplate, createTemplateForEditEmployeeForm } from './components/edit-employee-panel';
 
+const render = (container, template, position) => {
+  container.insertAdjacentHTML(position, template);
+}
+
+const renderComponent = (container, template, position = 'beforeend') => {
+  container.insertAdjacentHTML(position, template.render());
+}
+
 export const renderApp = () => {
   const rootContainer = document.querySelector('.js-work-display');
-  render(rootContainer, createCanvasTemplate(), 'afterbegin');
+  const canvas = new Canvas();
+  renderComponent(rootContainer, canvas);
 
   const violationInformation = [{ name: 'Миронов И.А', zone: 'Цех 1' }, { name: 'Петухов В.П.', zone: 'Высотные работы' }, { name: 'Лукин В.Р', zone: 'Цех 1' }];
-  
-  render(rootContainer, createNotificationListTemplate(violationInformation), 'beforeend');
-  /*render(rootContainer, createTemplateForOpenEmployeeListButton(), 'beforeend');*/
+  const notifications = new Notification(violationInformation);
+  renderComponent(rootContainer, notifications);
 
-  const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
+  const openEmployeeListPanelButton = new OpenEmployeeListPanelButton();
+  renderComponent(rootContainer, openEmployeeListPanelButton);
+
+  /*const employeesList = [{ id: 1, name: 'Пиратов В.К.' }, { id:  2, name: 'Шиханов П.А.'}, { id: 3, name: 'Терёхин У.Л.' }];
   render(rootContainer, createTemplateForEmployeesPanel(employeesList), 'beforeend');
 
   const employeePanel = rootContainer.querySelector('.js-employee-information-panel');
   const tracks = [{ id: 1, name: 1, points: [{}, {}]}, { id: 3, name: 3, points: [{}, {}]}, { id: 4, name: 4, points: [{}, {}]}];
   const zones = [ { id: 1, name: 'Цех 1' }, { id: 2, name: 'Высотные работы' }, { id: 3, name: 'Цех 2' }, { id: 4, name: 'Напряжение' } ];
-  /*const formAddEmployee = createTemplateForAddEmployeeForm(tracks, zones);
+  const formAddEmployee = createTemplateForAddEmployeeForm(tracks, zones);
 
-  render(employeePanel, createAddEmployeePanelTemplate(formAddEmployee), 'beforeend');*/
+  render(employeePanel, createAddEmployeePanelTemplate(formAddEmployee), 'beforeend');
 
   const employee = {
     id: 1,
@@ -32,9 +44,5 @@ export const renderApp = () => {
   };
   const formEditEmloyee = createTemplateForEditEmployeeForm(employee, tracks, zones, true);
 
-  render(employeePanel, createEditEmployeePanelTemplate(formEditEmloyee), 'beforeend');
-}
-
-const render = (container, template, position) => {
-  container.insertAdjacentHTML(position,template);
+  render(employeePanel, createEditEmployeePanelTemplate(formEditEmloyee), 'beforeend');*/
 }
