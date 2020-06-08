db73ef29dab3b5bdaeaf2ccf1159338ba5e40fd3
Lyashka <iuliana9864@gmail.com>
2020-06-08 20:26:41 +0300

    add import for Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,17 @@
+import Two from 'two.js';
 import { Component } from './Component';
+import { Wall } from '../Canvas/Wall';
+import { Door } from '../Canvas/Door';
+import { DrawableZone } from '../Canvas/DrawableZone';
+import { DrawableEmployee } from '../Canvas/DrawableEmployee';
+
+import {
+  WIDTH_CANVAS,
+  HEIGHT_CANVAS,
+  ELEMENTS_BUILDING,
+  EMPLOYEE_COLOR,
+  EMPLOYEE_RADUIS,
+} from '../const';
 
 const createCanvasTemplate = () =>`
   <canvas class="work-display__plan js-building-canvas">
