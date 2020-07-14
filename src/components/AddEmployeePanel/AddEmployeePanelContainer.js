6e4ef827e1e42354f51e8ac9c9dacf58c8b23834
Lyashka <iuliana9864@gmail.com>
2020-07-14 10:20:44 +0300

    change containers for add and edit employee

--- src/components/AddEmployeePanel/AddEmployeePanelContainer.js
+++ src/components/AddEmployeePanel/AddEmployeePanelContainer.js
@@ -3,7 +3,12 @@ import { Container } from '../Container';
 import { markOccupiedTracks } from '../../renderApp';
 
 import {
+  HIDE_EMPLOYEE_LIST_PANEL,
+  OPEN_ADD_PANEL,
+  EMPLOYEE_ADDED,
+  OPEN_EDIT_PANEL,
   EMPLOYEE_EDITED,
+  EMPLOYEE_REMOVED,
 } from '../../eventConstants';
 
 import {
@@ -21,6 +26,47 @@ export class AddEmployeePanelContainer extends Container {
     this.eventManager = eventManager;
     this.employeeApiService = employeeApiService;
     this.component = new AddEmployeePanelView({ tracks, zones });
+    this.component.setCloseButtonHandler(() => {
+      this.component.clearForm();
+      this.component.hide();
+    });
+
+    this.component.setAddEmployeeButtonHandler(async (event) => {
+      try {
+        event.preventDefault();
+        const newEmployee = this.component.getNewEmployee();
+        await this.employeeApiService.createEmployee(newEmployee);
+        eventManager.publish({
+          type: EMPLOYEE_ADDED,
+          payload: {
+            newEmployee,
+          },
+        });
+        const employeeList = await this.employeeApiService.getEmployees();
+        const tracksWithoutAddedEmployeeTrack = markOccupiedTracks(employeeList, EMPLOYEE_TRACKS);
+        this.component.setState({ tracks: tracksWithoutAddedEmployeeTrack, zones });
+        this.component.clearForm();
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    this.eventManager.subscribe(HIDE_EMPLOYEE_LIST_PANEL, () => {
+      if (this.component.isComponentShown()) {
+        this.component.hide();
+      }
+    });
+
+    this.eventManager.subscribe(OPEN_ADD_PANEL, () => {
+      this.component.show();
+    });
+
+    this.eventManager.subscribe(OPEN_EDIT_PANEL, () => {
+      if (this.component.isComponentShown) {
+        this.component.hide();
+      }
+    });
+
     this.eventManager.subscribe(EMPLOYEE_EDITED, async () => {
       try {
         const newEmployeeList = await this.employeeApiService.getEmployees();
@@ -31,5 +77,36 @@ export class AddEmployeePanelContainer extends Container {
         console.error(error);
       }
     });
+
+    this.eventManager.subscribe(EMPLOYEE_REMOVED, async () => {
+      try {
+        const newEmployeeList = await this.employeeApiService.getEmployees();
+        const tracksWithoutRemovedEmployeeTrack = markOccupiedTracks(newEmployeeList, EMPLOYEE_TRACKS);
+        this.component.setState({ tracks: tracksWithoutRemovedEmployeeTrack });
+        this.component.hide();
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+
+  getTemplate() {
+    return this.component.getTemplate();
+  }
+
+  getElement() {
+    return this.component.getElement();
+  }
+
+  show() {
+    return this.component.show();
+  }
+
+  hide() {
+    this.component.hide();
+  }
+
+  isComponentShown() {
+    this.component.isComponentShown();
   }
 }
--- src/components/EmployeeListPanel/EmployeeListPanelContainer.js
+++ src/components/EmployeeListPanel/EmployeeListPanelContainer.js
@@ -1,10 +1,20 @@
-/*eslint-disable*/
 import { Container } from '../Container';
 import { EmployeeListPanelView } from './EmployeeListPanelView';
+import { markOccupiedTracks, cloneDeep } from '../../renderApp';
 
 import {
+  HIDE_OPEN_EMPLOYEE_LIST_BUTTON,
+  HIDE_EMPLOYEE_LIST_PANEL,
+  OPEN_ADD_PANEL,
+  OPEN_EDIT_PANEL,
   EMPLOYEE_ADDED,
+  EMPLOYEE_EDITED,
+  EMPLOYEE_REMOVED,
 } from '../../eventConstants';
+
+import {
+  EMPLOYEE_TRACKS,
+} from '../../const';
 
 export class EmployeeListPanelContainer extends Container {
   constructor({
@@ -16,26 +26,80 @@ export class EmployeeListPanelContainer extends Container {
     this.eventManager = eventManager;
     this.employeeApiService = employeeApiService;
     this.component = new EmployeeListPanelView({ employeeList });
-    /*this.eventManager.subscribe(EMPLOYEE_ADDED, async () => {
+
+    this.eventManager.subscribe(HIDE_OPEN_EMPLOYEE_LIST_BUTTON, () => {
+      this.component.show();
+    });
+
+    this.eventManager.subscribe(EMPLOYEE_ADDED, async () => {
       try {
         const employeeListWithNewEmployee = await this.employeeApiService.getEmployees();
         this.component.setState({ employeeList: employeeListWithNewEmployee });
       } catch (error) {
         console.error(error);
       }
-    });*/
+    });
+
+    this.eventManager.subscribe(EMPLOYEE_EDITED, async () => {
+      try {
+        const newEmployeeList = await this.employeeApiService.getEmployees();
+        this.component.setState({ employeeList: newEmployeeList });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    this.eventManager.subscribe(EMPLOYEE_REMOVED, async () => {
+      try {
+        const newEmployeeList = await this.employeeApiService.getEmployees();
+        this.component.setState({ employeeList: newEmployeeList });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    this.component.setCloseButtonHandler(() => {
+      this.component.hide();
+      eventManager.publish({
+        type: HIDE_EMPLOYEE_LIST_PANEL,
+      });
+    });
+
+    this.component.setHandlerForAddPanelOpenButton(() => {
+      eventManager.publish({
+        type: OPEN_ADD_PANEL,
+      });
+    });
+
+    this.component.setHandlerForEditPanelOpenButton((event) => {
+      const employeeIdForEdit = event.target.id;
+      const employeeForEdit = cloneDeep(this.component.getCurrentEmployeeList().find((employee) => employee.id === employeeIdForEdit));
+      const tracksWithOccupiedStatus = markOccupiedTracks(this.component.getCurrentEmployeeList(), EMPLOYEE_TRACKS);
+  
+      eventManager.publish({
+        type: OPEN_EDIT_PANEL,
+        payload: {
+          employeeForEdit,
+          tracksWithOccupiedStatus,
+        },
+      });
+    });
   }
 
   getTemplate() {
-    this.component.getTemplate();
+    return this.component.getTemplate();
   }
 
   getElement() {
-    this.component.getElement();
+    return this.component.getElement();
   }
 
   show() {
-    this.component.show();
+    return this.component.show();
+  }
+
+  hide() {
+    this.component.hide();
   }
 
   /*setState(nextData) {
@@ -44,10 +108,6 @@ export class EmployeeListPanelContainer extends Container {
 
   rerender() {
     this.component.rerender();
-  }
-
-  hide() {
-    this.component.hide();
   }
 
   removeElement() {
